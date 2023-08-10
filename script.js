dea674d0b630664b67e2eac286c0cf35edc8e3fe
Carl Barcenas <carlanthony.barcenas@marquette.edu>
2023-08-10 13:46:10 -0500

    Functionalities finished. Will return to add finishing touches eventually

--- script.js
+++ script.js
@@ -1,13 +1,67 @@
-// Initialize 16x16 grid
-let grid = document.querySelector('.grid'); // Select grid as parent node
-for(let i = 0; i < 16; i++) {
-    console.log(`Row ${i}: \r`);
-    let currRow = document.createElement('div');
-    currRow.setAttribute("class", `row row_${i}`);
-    for(let j = 0; j < 16; j++) {
-        let currCol = document.createElement('div');
-        currCol.setAttribute("class", `col col_${j}`);
-        currRow.appendChild(currCol);
+const DEFAULT_SIZE = 16;
+const DEFAULT_COLOR = '#333333';
+let size = DEFAULT_SIZE;
+
+// Function to Initialize Grid
+function initializeGrid(size) {
+    // Creating Grid
+    let grid = document.querySelector('.grid'); // Select grid as parent node
+    for (let i = 0; i < size; i++) {                                // Rows
+        let currRow = document.createElement('div');
+        currRow.setAttribute("class", `row row_${i}`);
+        for (let j = 0; j < size; j++) {                            // Cols
+            let currCol = document.createElement('div');
+            currCol.setAttribute("class", `col col_${j}`);
+            currCol.addEventListener('mouseover', activateBlock);   // Listeners for drawing
+            currCol.addEventListener('mousedown', activateBlock);
+            currRow.appendChild(currCol);
+        }
+        grid.appendChild(currRow);
     }
-    grid.appendChild(currRow);
+}
+
+
+// Function for drawing
+let mouseDown = false
+document.body.onmousedown = () => (mouseDown = true)
+document.body.onmouseup = () => (mouseDown = false)
+function activateBlock(e) {
+    if(e.type === 'mouseover' && !mouseDown) return;
+    e.target.style.backgroundColor = DEFAULT_COLOR;
+}
+
+
+// Function to add rainbow coloring [TODO]
+const rainbowBtn = document.querySelector('.rainbow_btn');
+rainbowBtn.addEventListener('click', rainbowPencil);
+function rainbowPencil(e) {
+    console.log('test');
+    return 0;
+}
+
+
+// Function for Resizing
+let sliderval = document.querySelector('.slider_val');
+let slider = document.querySelector('.slider');
+slider.addEventListener('change', function() {
+    sliderval.innerHTML = slider.value;
+    clearBoard();
+    size = slider.value;
+    initializeGrid(slider.value);
+})
+
+
+// Function for Clearing
+const clearbtn = document.querySelector('.clc_btn');
+clearbtn.addEventListener('click', clearBoard);
+let grid = document.querySelector('.grid');
+function clearBoard() {
+    grid.innerHTML = "";
+    initializeGrid(size);
+}
+
+
+// Load Application
+window.onload = () => {
+    initializeGrid(DEFAULT_SIZE);
 }
